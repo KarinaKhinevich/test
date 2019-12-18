c59592a2c52ac0084a5837251bed12575f8f7980
MareTenebrarum <44246233+MareTenebrarum@users.noreply.github.com>
2019-12-18 23:39:22 +0300

    Add files via upload

---
+++ f2py.py
@@ -0,0 +1,4 @@
+#!c:\users\user\appdata\local\programs\python\python37\python.exe
+# EASY-INSTALL-SCRIPT: 'numpy==1.15.4','f2py.py'
+__requires__ = 'numpy==1.15.4'
+__import__('pkg_resources').run_script('numpy==1.15.4', 'f2py.py')
